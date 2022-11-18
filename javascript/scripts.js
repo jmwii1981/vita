575b8b46b72ba8c649cdbe7e9254dbfa7628431f
Jan Michael Wallace <hello@janmichael.io>
2022-11-17 20:23:44 -0500

    scripts.js file creates sentence object and injects it into document properly

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -5,60 +5,69 @@
 
 */
 
-// API Data Grab
+// Data
 import { githubRepoUrlFull, githubCommitsUrlFull, numberOfCommits } from './data/github.mjs';
-import { bookTitle, bookLink, listOfAuthors } from './data/openLibrary.mjs';
+import { listOfAuthors, bookLink, bookTitle } from './data/openLibrary.mjs';
 import { searchUrlFull, currentShoes, currentGear } from './data/openWeather.mjs';
-import { trackName, trackURL, trackArtist, spotifyTrackURL, spotifyArtistURL } from './data/lastFm.mjs';
+import { trackURL, spotifyTrackURL, spotifyArtistURL, trackName, trackArtist } from './data/lastFm.mjs';
 
 // Factories
-// import { sentenceBuilderFactory } from './factories/sentenceBuilder.mjs';
-// import { addToDocumentFactory } from './factories/addToDocument.mjs';
+import { paragraphTagFactory } from './factories/paragraphTag.mjs';
+import { anchorTagFactory } from './factories/anchorTag.mjs';
+import { sentenceBuilderFactory } from './factories/sentenceBuilder.mjs';
+import { addToDocumentFactory } from './factories/addToDocument.mjs';
+
+// Create Objects for later use ...
+const linkedInLink = anchorTagFactory(`Jan Michael`, undefined, `//linkedin.com/in/jmwii1981`, undefined, undefined, undefined, undefined);
+const googleMapsLink = anchorTagFactory(`Louisville, KY`, undefined, `//www.google.com/maps/place/Louisville,+KY/@38.1884721,-85.9569664,10z/data=!3m1!4b1!4m6!3m5!1s0x88690b1ab35bd511:0xd4d3b4282071fd32!8m2!3d38.2526647!4d-85.7584557!16zL20vMGZfXzE`, undefined, undefined, undefined, undefined);
+const myWeatherShoesLink =  anchorTagFactory(`${currentShoes}`, undefined, `${searchUrlFull}`, undefined, undefined, undefined, undefined);
+const myWeatherGearLink =  anchorTagFactory(`${currentGear}`, undefined, `${searchUrlFull}`, undefined, undefined, undefined, undefined);
+const myBookLink = anchorTagFactory(`${bookTitle}`, undefined, `${bookLink}`, undefined, undefined, undefined, undefined);
+const myTrackLink = anchorTagFactory(`${trackName}`, undefined, `${spotifyTrackURL}`, undefined, undefined, undefined, undefined);
+const myArtistLink = anchorTagFactory(`${trackArtist}`, undefined, `${spotifyArtistURL}`, undefined, undefined, undefined, undefined);
 
 // Content
 const myOpening = [
-    `<p>`,
-    `<a class="link link--x-small link--light link--weight-900" href="//linkedin.com/in/jmwii1981" target="_blank">Jan Michael</a>`,
+    linkedInLink,
 ];
 const myLocation = [
     ` was last seen in `,
-    `<a class="link link--x-small link--light link--weight-900" href="//www.google.com/maps/place/Louisville,+KY/@38.1884721,-85.9569664,10z/data=!3m1!4b1!4m6!3m5!1s0x88690b1ab35bd511:0xd4d3b4282071fd32!8m2!3d38.2526647!4d-85.7584557!16zL20vMGZfXzE" target="_blank">Louisville, KY</a>, `,
+    googleMapsLink
 ];
 const myWeather = [
-    `sporting his `,
-    // topic: myWeatherShoesLink,
-    currentShoes,
+    ` sporting his `,
+    myWeatherShoesLink,
     ` and `,
-    // topic: myWeatherGearLink,
-    currentGear,
+    myWeatherGearLink,
     `, `,
 ];
 const myReading = [
     ` reading `,
-    `<a href="${bookLink}">${bookTitle}</a>`,
-    ` by `,
-    listOfAuthors,
+    myBookLink,
+    // ` by `,
+    // listOfAuthors,
 ];
 const myMusic = [
-    `and, `,
-    `vibing to `,
-    `<a id="myTrack" href="https://open.spotify.com/search/Track" target="_blank" class="link link--x-small link--light link--weight-900">
-                "Track"
-            </a>`,
+    `, and vibing to `,
+    myTrackLink,
     ` by `,
-    `<a id="myArtist" href="https://open.spotify.com/search/Artist" target="_blank" class="link link--x-small link--light link--weight-900">
-                Artist
-            </a>`,
+    myArtistLink,
     `.`,
 ];
-const myClosing = [
-    `</p>`,
-];
-const footerContent = document.getElementById(`footer-content`);
+
+// Put it all together ..
 let fullSentence = [];
-fullSentence.push(myOpening, myLocation, myWeather, myReading, myMusic, myClosing);
-fullSentence = fullSentence.flat().join(``);
-footerContent.append(fullSentence);
+fullSentence.push(myOpening, myLocation, myWeather, myReading, myMusic);
+fullSentence = fullSentence.flat();
+
+// Create p tag, flatten array, and append to p tag ...
+let mySentence = paragraphTagFactory(``);
+fullSentence.forEach(element => mySentence.append(element));
+
+// Append mySentence to footer-content div in document ...
+const footerContent = document.getElementById(`footer-content`);
+footerContent.append(mySentence);
+
 
 /*
     STATIC SENTENCE (BEGINNING)
